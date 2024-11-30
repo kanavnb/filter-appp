d3db1dd93d4ba05e05f27f63e0a632b079d19095
kanavnb <kanatbek.kamilov@alatoo.edu.kg>
2024-11-30 16:51:46 +0600

    added data initialization on the first render

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const DataFilter = () => {
-    const [data, setData] = useState([]); 
-    const [search, setSearch] = useState(''); 
+    const [data, setData] = useState([]);
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        const fetchData = () => {
+            const mockData = [
+                'Lion', 'Tiger', 'Elephant', 'Giraffe',
+                'Zebra', 'Kangaroo', 'Panda', 'Penguin',
+                'Koala', 'Leopard', 'Cheetah', 'Sloth'
+            ];
+            setData(mockData);
+        };
+
+        fetchData();
+    }, []);
+
     return <div>Data Filter Component</div>;
 };
 
