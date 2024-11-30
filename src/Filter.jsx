2af0d03dccc067b42b46b3a197426fcc556f6997
kanavnb <kanatbek.kamilov@alatoo.edu.kg>
2024-11-30 16:42:19 +0600

    added state for the data list and search string

---
+++ src/Filter.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const DataFilter = () => {
+    const [data, setData] = useState([]); 
+    const [search, setSearch] = useState(''); 
+    return <div>Data Filter Component</div>;
+};
+
+export default DataFilter;
